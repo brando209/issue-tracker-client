7b42d70607898902abecd7a87ce2977a4cf3ffb5
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-01-04 12:43:51 -0800

    Moved btns to right side of  IssueOverview card ui

--- src/components/app/IssueOverview/IssueOverview.js
+++ src/components/app/IssueOverview/IssueOverview.js
@@ -7,24 +7,15 @@ function IssueOverview({ projectId, issue, onDelete, onAssign }) {
     const handleAssignIssue = () => onAssign({ projectId: projectId, issueId: issue.id });
 
     return (
-        <Card style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'row' }}>
-            <Card.Body style={{ flex: 1 }}>
+        <Card style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'row', alignItems: "center" }}>
+            <Card.Body style={{ flex: 5, padding: 0 }}>
                 <Card.Title>{issue.title}</Card.Title>
                 <Card.Text>
                     {issue.description}
                 </Card.Text>
             </Card.Body>
 
-            <Card.Body style={{ flex: 1 }}>
-                <LinkButton variant="primary" to={`/projects/${projectId}/issues/${issue.id}`}>View</LinkButton>
-                <Button variant="primary" onClick={handleAssignIssue}>Assign</Button>
-                <DropdownButton variant="secondary" title="Settings">
-                    <Dropdown.Item >Edit Issue</Dropdown.Item>
-                    <Dropdown.Item onClick={handleDeleteIssue}>Delete Issue</Dropdown.Item>
-                </DropdownButton>
-            </Card.Body>
-
-            <Card.Body style={{ flex: 1, borderLeft: '1px solid black' }}>
+            <Card.Body style={{ flex: 1, height: "100%", borderLeft: '1px solid black' }}>
                 <Card.Title>Category</Card.Title>
                 <Card.Text>
                     {issue.category}
@@ -43,6 +34,22 @@ function IssueOverview({ projectId, issue, onDelete, onAssign }) {
                 <Card.Text>
                     {issue.status}
                 </Card.Text>
+            </Card.Body>
+
+            <Card.Body style={{ flex: 1, borderLeft: '1px solid black' }}>
+                <Card.Title>Assigned</Card.Title>
+                <Card.Text>
+                    {issue.assigneeId ? issue.assigneeId : "None"}
+                </Card.Text>
+            </Card.Body>
+
+            <Card.Body style={{ flex: 2, height: "100%", borderLeft: '1px solid black', padding: 0 }}>
+                <LinkButton variant="primary" to={`/projects/${projectId}/issues/${issue.id}`}>View</LinkButton>
+                <Button variant="primary" onClick={handleAssignIssue}>Assign</Button>
+                <DropdownButton variant="secondary" title="Settings">
+                    <Dropdown.Item >Edit Issue</Dropdown.Item>
+                    <Dropdown.Item onClick={handleDeleteIssue}>Delete Issue</Dropdown.Item>
+                </DropdownButton>
             </Card.Body>
         </Card>
     )
