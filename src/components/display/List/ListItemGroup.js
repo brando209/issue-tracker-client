0731d76b4e497a86eb9af21458bf5a89155ee6f2
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-01-10 15:07:50 -0800

    Allow groups of a List to be collapsible

--- src/components/display/List/ListItemGroup.js
+++ src/components/display/List/ListItemGroup.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from './ListItem';
 
 function ListItemGroup({ name, value, items, render }) {
+    const [collapse, setCollapse] = useState(false);
     return (
-        <div className={"list-item-group " + name}>
+        <div className={"list-item-group " + name} onClick={() => setCollapse(prev => !prev)}>
             <div className="group-header">{value}</div>
             {
+                collapse ? null :
                 items && items.map((item, idx) => <ListItem key={`${value}-${idx}`} item={item} render={render} />)
             }
         </div>
