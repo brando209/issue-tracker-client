440902b808671620e2eb531e69a353c57695e4e1
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-01-16 14:09:43 -0800

    Bug fix: collapse only when clicking group header

--- src/components/display/List/ListItemGroup.js
+++ src/components/display/List/ListItemGroup.js
@@ -4,8 +4,8 @@ import ListItem from './ListItem';
 function ListItemGroup({ name, value, items, render }) {
     const [collapse, setCollapse] = useState(false);
     return (
-        <div className={"list-item-group " + name} onClick={() => setCollapse(prev => !prev)}>
-            <div className="group-header">{value}</div>
+        <div className={"list-item-group " + name}>
+            <div className="group-header" onClick={() => setCollapse(prev => !prev)}>{value}</div>
             {
                 collapse ? null :
                 items && items.map((item, idx) => <ListItem key={`${value}-${idx}`} item={item} render={render} />)
