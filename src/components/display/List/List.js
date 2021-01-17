c7f35082fee592b9c88c844d530ad34f1d6177b4
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-01-16 16:40:18 -0800

    Made component filter by searchText (searchable)

--- src/components/display/List/List.js
+++ src/components/display/List/List.js
@@ -5,35 +5,49 @@ import ListItem from './ListItem'
 import ListItemGroup from './ListItemGroup';
 import './List.css';
 
-function List({ listItems, groupKey = null, groupValues = null, orderBy = 'asc', filter = null, render }) {
+function List({ listItems, groupKey = null, groupValues = null, orderBy = 'asc', filter = null, searchText = "", searchKeys = [], render }) {
     // Maps property keys of 'filter' prop to an array containing the values that are not being filtered out
     const allowedValues = mapValues(filter, (obj) => {
         const selectedFilters = pickBy(obj, (value) => value === true);
         return Object.keys(selectedFilters);
     });
+    
     const filteredItems = listItems && listItems.filter(item => {
         const allowedKeys = Object.keys(allowedValues);
         let isAllowed = true;
+        // Filter out items based on the current filter
         for(let key of allowedKeys) {
             if(includes(allowedValues[key], item[key]) === false) {
                 isAllowed = false;
             }
         }
+        // Also, filter out items not matching the current search (item['searchKeys'] !== 'searchText')
+        if(searchText !== "") {
+            for(let key of searchKeys) {
+                if(includes(item[key].toLowerCase(), searchText.toLowerCase()) === false) {
+                    isAllowed = false;
+                } else break;
+            }
+        }
         return isAllowed;
     });
+
     const groupedItems = groupBy(filteredItems, (value) => value[groupKey]);
 
     const listComponents = () => {
+        let listComponentArray;
         if(groupKey && groupValues) {
             const groupComponents = [];
             groupValues.forEach(value => {
                 const values = groupedItems[value];
                 groupComponents.push(<ListItemGroup key={`${groupKey}=${value}`} name={groupKey} value={value} items={values} render={render}/>);
             })
-            return orderBy === 'desc' ? groupComponents.reverse() : groupComponents;
+            listComponentArray = orderBy === 'desc' ? groupComponents.reverse() : groupComponents;
+        } else {
+            listComponentArray = filteredItems && filteredItems.map((item, idx) => <ListItem item={item} key={idx} keyValue={idx} render={render} />);
         }
 
-        return filteredItems && filteredItems.map((item, idx) => <ListItem item={item} key={idx} keyValue={idx} render={render} />);
+        return listComponentArray;
     }
 
     return (
