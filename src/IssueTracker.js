d0ccfbc5a006b29790035396ddeca8237b0da4d7
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-02-06 15:37:34 -0800

    Update route hierarchy to allow moving up state

--- src/IssueTracker.js
+++ src/IssueTracker.js
@@ -7,13 +7,8 @@ import TopNavBar from './components/app/Navigation/TopNavBar';
 
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
-import NewProjectPage from './pages/NewProjectPage/NewProjectPage';
-import NewIssuePage from './pages/NewIssuePage/NewIssuePage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import ProjectDashboard from './pages/ProjectDashboard/ProjectDashboard';
-import IssueDashboard from './pages/IssueDashboard/IssueDashboard';
-import ProjectDetails from './pages/ProjectDetails/ProjectDetails';
-import IssueDetails from './pages/IssueDetails/IssueDetails';
 
 function IssueTracker() {
 
@@ -25,11 +20,6 @@ function IssueTracker() {
                     <Route path="/login" component={LoginPage} />
                     <Route path="/signup" component={SignupPage} />
                     <PrivateRoute path="/dashboard" component={Dashboard} />
-                    <PrivateRoute path="/projects/:projectId/issues/new" component={NewIssuePage} />
-                    <PrivateRoute path="/projects/:projectId/issues/:issueId" component={IssueDetails} />
-                    <PrivateRoute path="/projects/:projectId/issues" component={IssueDashboard} />
-                    <PrivateRoute path="/projects/new" component={NewProjectPage} />
-                    <PrivateRoute path="/projects/:projectId/" component={ProjectDetails} />
                     <PrivateRoute path="/projects" component={ProjectDashboard} />
                     <Route path="/">
                         <div>Home</div>
--- src/pages/ProjectDashboard/ProjectDashboard.js
+++ src/pages/ProjectDashboard/ProjectDashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import useAuth from '../../hooks/useAuth';
@@ -7,12 +8,18 @@ import useResource from '../../hooks/useResource';
 import ProjectsNavBar from '../../components/app/Navigation/ProjectsNavBar';
 import ProjectList from '../../components/app/ProjectList/ProjectList';
 
+import NewProjectPage from '../../pages/NewProjectPage/NewProjectPage';
+import NewIssuePage from '../../pages/NewIssuePage/NewIssuePage';
+import IssueDashboard from '../../pages/IssueDashboard/IssueDashboard';
+import ProjectDetails from '../../pages/ProjectDetails/ProjectDetails';
+import IssueDetails from '../../pages/IssueDetails/IssueDetails';
+
 import projectsApi  from '../../api/projects';
 import useDialogBox from '../../hooks/useDialogBox';
 import SelectForm from '../../components/form/SelectForm';
 
 
-function ProjectDashboard() {
+function ProjectDashboard({ match }) {
     const auth = useAuth();
     const projects = useResource('http://localhost:3001/api/projects', auth.user ? auth.user.token : null);
     const collaborators = useResource('http://localhost:3001/api/user/all', auth.user ? auth.user.token : null);
@@ -55,13 +62,24 @@ function ProjectDashboard() {
                     />
                 )}
             />
-            <ProjectsNavBar />
-            <ProjectList 
-                projectList={projects.data} 
-                onDelete={showDeleteProjectDialogBox} 
-                onEdit={handleEditProject} 
-                onAddCollaborator={showAddCollaboratorDialogBox} 
-            />
+            <Switch>
+                <Route path={match.url} exact render={() => (
+                    <>
+                        <ProjectsNavBar />
+                        <ProjectList 
+                            projectList={projects.data} 
+                            onDelete={showDeleteProjectDialogBox} 
+                            onEdit={handleEditProject} 
+                            onAddCollaborator={showAddCollaboratorDialogBox} 
+                        />
+                    </>
+                )}/>
+                <Route path={`${match.url}/:projectId/issues/new`} exact component={NewIssuePage} />
+                <Route path={`${match.url}/:projectId/issues/:issueId`} exact component={IssueDetails} />
+                <Route path={`${match.url}/:projectId/issues`} exact component={IssueDashboard} />
+                <Route path={`${match.url}/new`} exact component={NewProjectPage} />
+                <Route path={`${match.url}/:projectId/`} exact component={ProjectDetails} />
+            </Switch>
         </Container>
     )
 }
