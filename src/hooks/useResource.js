77e7fe767cdc57af68c0a69ca96c4cc5726d60b0
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-04-24 18:58:00 -0700

    Modify useResource hook to hit mult endpts

--- src/hooks/useResource.js
+++ src/hooks/useResource.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function useResource(path, authToken) {
+function useResource(path, authToken, forkedPaths = []) {
     const [resource, setResource] = useState({
         data: [],
         isLoading: false,
@@ -15,8 +15,20 @@ function useResource(path, authToken) {
             setResource({ data: [], isLoading: true, isError: false });
             try {
                 const headers = authToken ? { 'Authorization': `Bearer ${authToken}` } : {}
-                const response = await axios.get(path, { headers });
-                setResource({ data: response.data, isLoading: false, isError: false });
+                let response;
+                if(forkedPaths.length > 0) {
+                    const promises = [];
+                    forkedPaths.forEach(fork => {
+                        promises.push(axios.get(path + "/" + fork, { headers }));
+                    });
+                    response = await Promise.all(promises);
+                    response = response.map(obj => obj.data);
+                    setResource({ data: response, isLoading: false, isError: false });
+                } else {
+                    response = await axios.get(path, { headers });
+                    setResource({ data: response.data, isLoading: false, isError: false });
+                }
+
             } catch(err) {
                 console.log(err);
                 setResource({ data: err, isLoading: false, isError: true })
